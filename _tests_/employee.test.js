77cbeb8beee5d164a15e9b910b3c4020811b24d3
Kyle Schoppmann <klparuu@yahoo.com>
2023-01-20 14:48:26 -0700

    wrote test for employee.js file

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -8,4 +8,68 @@ describe("Employee", () => {
             expect(typeof obj).toEqual("object");
         });
     })
+
+    describe("name", () => {
+        it("should return a name", () => {
+            const name = "David";
+            const obj = new Employee(name)
+
+            expect(obj.name).toEqual(name);
+        });
+    })
+
+    describe("id", () => {
+        it("should return an ID", () => {
+            const id = 1;
+            const obj = new Employee("David", id)
+
+            expect(obj.id).toEqual(id);
+        });
+    })
+
+    describe("email", () => {
+        it("should return an email", () => {
+            const email = "david@email.com";
+            const obj = new Employee("David", 1, email)
+
+            expect(obj.email).toEqual(email);
+        });
+    })
+
+    describe("getName", () => {
+        it("should return a name from getName()", () => {
+            const name = "David";
+            const obj = new Employee(name)
+
+            expect(obj.getName()).toEqual(name);
+        });
+    })
+
+    describe("getId", () => {
+        it("should return the ID from getId()", () => {
+            const id = 1;
+            const obj = new Employee("David", id)
+
+            expect(obj.getId()).toEqual(id);
+        });
+    })
+
+    describe("getEmail", () => {
+        it("should return a email from getEmail()", () => {
+            const email = "david@email.com";
+            const obj = new Employee("David", 1, email)
+
+            expect(obj.getEmail()).toEqual(email);
+        });
+    })
+
+    describe("getRole", () => {
+        it("should return the employee's role from getRole()", () => {
+            const role = "Employee";
+            const obj = new Employee()
+
+            expect(obj.getRole()).toEqual(role);
+        });
+    })
+
 })
